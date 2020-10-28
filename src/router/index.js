40fccab6a7e8b93ca5e4f39a29de3952031ef17c
杨帆 <158533215@qq.com>
2020-10-28 20:13:04 +0800

    第一天

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import Register from "@/views/Register";
+import Search from "@/views/Search";
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/home",
+      component: Home,
+    },
+    {
+      path: "/login",
+      component: Login,
+      meta: {
+        isHidden: true,
+      },
+    },
+    {
+      path: "/register",
+      component: Register,
+      meta: {
+        isHidden: true,
+      },
+    },
+    {
+      path: "/search/:keyword?",
+      component: Search,
+        name: "search",
+        props: (route) => ({keyword1:route.query.keyword1,keyword:route.params.keyword})
+    },
+    {
+      path: "/",
+      redirect: "/home",
+    },
+  ],
+});
---
+++ src/views/Search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      Search
+      params参数:{{keyword}}
+      <br><br>
+      query参数:{{keyword1}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  props:['keyword1','keyword']
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
